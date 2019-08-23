9a14127b1c59c2c89deb0c220cb8d853f9302ba5
dushiyi <dushiyi319@163.com>
2019-08-23 10:55:00 +0800

    some changes

---
+++ SnS_pipeline/database/work.sh
@@ -0,0 +1,4 @@
+makeblastdb  -parse_seqids  -dbtype  nucl  -in  SplitCDR3JOligosA.CDR3.fa
+makeblastdb  -parse_seqids  -dbtype  nucl  -in  SplitCDR3JOligosB.CDR3.fa
+makeblastdb  -parse_seqids  -dbtype  nucl  -in  SplitCDR3JOligos.zip.fa
+
