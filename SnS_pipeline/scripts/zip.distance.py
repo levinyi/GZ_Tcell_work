78237c0577adef7a2f5dac6047f440e07822d0eb
dushiyi <dushiyi319@163.com>
2019-08-26 17:29:59 +0800

    some changes

--- SnS_pipeline/scripts/zip.distance.py
+++ SnS_pipeline/scripts/zip.distance.py
@@ -2,9 +2,12 @@ import sys
 sys.path.append("/cygene/script/python_packages")
 import edit_distance
 
-with open(sys.argv[1],"r") as f:
+with open(sys.argv[1], "r") as f:
     for line in f:
         line = line.rstrip("\n")
-        reads,refa,refb,zipa,zipb,flag = line.split("\t")
-        distance = edit_distance.minEditDist(zipa,zipb)
-        print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(reads,refa,refb,zipa,zipb,flag,distance))
+        reads, refa, refb, zipa, zipb, flag, zipr = line.split("\t")
+        distance1 = edit_distance.minEditDist(zipa, zipb)
+        distance2 = edit_distance.minEditDist(zipa, zipr)
+        distance3 = edit_distance.minEditDist(zipb, zipr)
+        print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(reads, refa, refb, zipa, zipb, flag, distance1, zipr, distance2, distance3))
+
