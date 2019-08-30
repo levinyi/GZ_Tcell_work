81d9af854d91d2d09946b7b64580e6e13642e740
dushiyi <dushiyi319@163.com>
2019-08-30 09:28:51 +0800

    some changes

---
+++ TCR_Library_1_pipeline/scripts/UMI.heatmap.geom_raster.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+args= commandArgs(T)
+data = read.table(args[1], header = F)
+name = args[2]
+ggplot(data, aes(x=reorder(data$V1,-data$V4), y=reorder(data$V2,data$V5), fill=log(data$V3,2))) + 
+	geom_raster() + ggtitle(name)+
+	xlab("UMIs") + ylab("clonotype")+
+	theme(legend.title = element_blank())
+output = paste(name,".umi.raster.jpg",sep=".")
+ggsave(output)
+print(output)
