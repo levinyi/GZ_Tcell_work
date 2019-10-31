bf10d1af137db80e433509e6bb6003302353f162
dushiyi <dushiyi319@163.com>
2019-10-31 15:37:24 +0800

    some changes

--- 10xGenomics/scripts/generate.matrix.py
+++ 10xGenomics/scripts/generate.matrix.py
@@ -1,11 +1,23 @@
-import sys
+import csv
+import gzip
 import os
+import scipy.io
+from itertools import islice 
+ 
+matrix_dir = "./"
+# mat = scipy.io.mmread(os.path.join(matrix_dir, "matrix.mtx.gz"))
 
-barcode_file = sys.argv[1]
-feature_file = sys.argv[2]
-matrixs_file = sys.argv[3]
 
+features_path = os.path.join(matrix_dir, "features.tsv.gz")
+feature_ids = [row[0] for row in csv.reader(gzip.open(features_path), delimiter="\t")]
+gene_names = [row[1] for row in csv.reader(gzip.open(features_path), delimiter="\t")]
+feature_types = [row[2] for row in csv.reader(gzip.open(features_path), delimiter="\t")]
 
-with open(barcode_file, "r") as f:
-    for line in f:
-        line = line.rstrip("\n")
+barcodes_path = os.path.join(matrix_dir, "barcodes.tsv.gz")
+barcodes = [row[0] for row in csv.reader(gzip.open(barcodes_path), delimiter="\t")]
+
+with gzip.open("matrix.mtx.gz","r") as f:
+    for line in islice(f,3,None):
+        a,b,c = line.split()
+        print("{}\t{}\t{}\t{}\t{}".format(feature_ids[int(a)-1], gene_names[int(a)-1], feature_types[int(a)-1], barcodes[int(b)-1], c))
+        
