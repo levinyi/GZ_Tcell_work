5ddb9005f2bf46cee0621cb63537447c332c03b3
dushiyi <dushiyi319@163.com>
2019-11-04 09:04:27 +0800

    some changes

--- 10xGenomics/scripts/generate.matrix.py
+++ 10xGenomics/scripts/generate.matrix.py
@@ -16,8 +16,29 @@ feature_types = [row[2] for row in csv.reader(gzip.open(features_path), delimite
 barcodes_path = os.path.join(matrix_dir, "barcodes.tsv.gz")
 barcodes = [row[0] for row in csv.reader(gzip.open(barcodes_path), delimiter="\t")]
 
+def addtwodimdict(thedict, key_a, key_b, val):
+    ''' this is a function to add two dimetion dict '''
+    if key_a in thedict:
+        thedict[key_a].update({key_b: val})
+    else:
+        thedict.update({key_a:{key_b: val}})
+    return thedict
+
+big_dict = {}
+gene_list = []
+barcode_list = []
 with gzip.open("matrix.mtx.gz","r") as f:
     for line in islice(f,3,None):
         a,b,c = line.split()
-        print("{}\t{}\t{}\t{}\t{}".format(feature_ids[int(a)-1], gene_names[int(a)-1], feature_types[int(a)-1], barcodes[int(b)-1], c))
-        
+        addtwodimdict(big_dict, feature_ids[int(a)-1], barcodes[int(b)-1], c)
+        gene_list.append(feature_ids[int(a)-1])
+        barcode_list.append(barcodes[int(b)-1])
+        # print("{}\t{}\t{}\t{}\t{}".format(feature_ids[int(a)-1], gene_names[int(a)-1], feature_types[int(a)-1], barcodes[int(b)-1], c))
+
+
+print "\t","\t".join(barcode_list)
+for i in xrange(len(gene_list)):
+    print gene_list[i],
+    for j in xrange(len(barcode_list)):
+        print big_dict[gene_list[i]].get(barcode_list[j],0),"\t",
+    print ""
