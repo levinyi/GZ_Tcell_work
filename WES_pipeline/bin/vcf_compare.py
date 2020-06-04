3518fc48b7ce2b80b2f7f9210bb73d8c58039cf6
levin <dushiyi319@163.com>
2020-06-04 13:48:59 +0800

    some changes

--- WES_pipeline/bin/vcf_compare.py
+++ WES_pipeline/bin/vcf_compare.py
@@ -11,14 +11,20 @@ def usage():
 def deal_vcf_file(avcf):
     adict = {}
     alist = []
-    with open(avcf, "r") as f:
-        for line in f:
-            if line.startswith("#"):
-                continue
-            a = line.split("\t")
-            identity = a[0].replace("chr", "") + '_' + a[1]
-            alist.append(identity)
-            adict[identity] = a
+    if avcf.endswith(".gz"):
+        import gzip
+        handle = gzip.open(avcf, "rb")
+        print("yes gzip")
+    else:
+        handle = open(avcf, "r")
+
+    for line in handle:
+        if line.startswith("#"):
+            continue
+        a = line.split("\t")
+        identity = a[0] + '_' + a[1]
+        alist.append(identity)
+        adict[identity] = a
     return alist, adict
 
 
@@ -28,27 +34,31 @@ file0 = sys.argv[1]
 # file1 = "/cygene/work/P0000-Blackbird/CR001-GA001_ByNovogene/CR001001_TU_gDNA.merged.xls"
 file1 = sys.argv[2]
 
-output1 = open("shared_but_not_pass.xls", "w")
-output2 = open("shared_and_pass.xls", "w")
-output3 = open("overdetected.xls", "w")
-output4 = open("not_detected.xls", "w")
+# output1 = open("shared_but_not_pass.xls", "w")
+# output2 = open("shared_and_pass.xls", "w")
+# output3 = open("overdetected.xls", "w")
+# output4 = open("not_detected.xls", "w")
 
 vcf1_list, vcf1_dict = deal_vcf_file(file0)
 vcf2_list, vcf2_dict = deal_vcf_file(file1)
 
-new_list = list(set(vcf_list).union(xls_list))
+# new_list = list(set(vcf1_list).union(vcf2_list))
+new_list = list(set(vcf1_list).intersection(vcf2_list))
 
 for each in new_list:
-    if each in vcf_list and each in vcf_list:
-        if vcf_dict[each][6] == "PASS":
-            output2.write("{}\t{}\t{}\n".format(each,vcf_dict[each][9].split(":")[2], xls_dict[each][41].split(":")[4]))
+    print("{}\t{}".format(vcf1_dict[each], vcf2_dict[each]))
+    '''
+    if each in vcf1_list and each in vcf2_list:
+        if vcf1_dict[each][6] == "PASS":
+            output2.write("{}\t{}\t{}\n".format(each,vcf1_dict[each][9].split(":")[2], vcf2_dict[each][41].split(":")[4]))
         else:
-            output1.write("{}\t{}\t{}\n".format(each,vcf_dict[each][9].split(":")[2], xls_dict[each][41].split(":")[4]))
-    if each in vcf_list and each not in xls_list:
-        output3.write("{}\t{}\n".format(each,vcf_dict[each][9].split(":")[2] ))
-    if each not in vcf_list and each in xls_list:
-        output4.write("{}\t{}\n".format(each,xls_dict[each][41].split(":")[4]))
+            output1.write("{}\t{}\t{}\n".format(each,vcf1_dict[each][9].split(":")[2], vcf2_dict[each][41].split(":")[4]))
+    if each in vcf1_list and each not in vcf2_list:
+        output3.write("{}\t{}\n".format(each,vcf1_dict[each][9].split(":")[2] ))
+    if each not in vcf1_list and each in vcf2_list:
+        output4.write("{}\t{}\n".format(each,vcf2_dict[each][41].split(":")[4]))
 output1.close()
 output2.close()
 output3.close()
 output4.close()
+'''
