5cf877204738a9a991c3da0558e6979da064c9f3
levin <dushiyi319@163.com>
2020-08-04 15:53:56 +0800

    some changes

--- Shotgun_TCRseq/bin/0.read2barcode.py
+++ Shotgun_TCRseq/bin/0.read2barcode.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import gzip
 from Bio import SeqIO
 
@@ -7,11 +7,16 @@ def usage():
     print("""
     usage:
         python {0} <barcode_file> <reads_file> <result_file_prefix>  
-
+        
+    example:
+        python {0} /cygene/work/00.test/pipeline/Shotgun_TCRseq/database/Shotgun_TCRseq.TRB.Barcodes.txt\
+                reads_file 
+q
     update:
+    20200723: change output format, add a new column for Barcode seq.
     20200604: updated.
     20200302: created.
-    """.format(sys.argv[0]))
+    """.format(os.path.basename(sys.argv[0])))
 
 
 def main():
@@ -24,15 +29,17 @@ def main():
     prefix_name = sys.argv[3]
 
     barcode_dict = {}
+    barcode_dict_reverse = {}
     with open(barcode_file, "r") as f:
         for line in f:
             line = line.rstrip("\n")
-            bc_name, bc_seq = line.split("\t")
+            bc_name, bc_seq = line.split()
             barcode_dict[bc_seq] = bc_name
+            barcode_dict_reverse[bc_name] = bc_seq
+
 
     barcode2reads_dict = {}
     total_reads = 0
-    
     if reads_file.endswith(".gz"):
         fastq_file = gzip.open(reads_file, "rt")
     else:
@@ -51,9 +58,13 @@ def main():
                 barcode2reads_dict[bc_name] += 1
 
     with open(prefix_name + ".barcode.total.rate.xls", "w") as f:
-        for each in barcode2reads_dict:
-            count = barcode2reads_dict[each]
-            f.write("{}\t{}\t{}\t{}\n".format(each,count,total_reads, count/float(total_reads)))
+        for bc_name in barcode2reads_dict:
+            count = barcode2reads_dict[bc_name]
+            f.write("{}\t{}\t{}\t{}\t{}\n".format(bc_name,
+                barcode_dict_reverse[bc_name],
+                count,
+                total_reads,
+                count/float(total_reads)))
     output.close()
 
 if __name__ == '__main__':
