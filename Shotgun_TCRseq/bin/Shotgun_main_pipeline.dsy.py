b8c8b84c48a73eb93edd9568d5d0b41f987bd5b6
levin <dushiyi319@163.com>
2020-08-06 09:03:07 +0800

    some changes

--- Shotgun_TCRseq/bin/Shotgun_main_pipeline.dsy.py
+++ Shotgun_TCRseq/bin/Shotgun_main_pipeline.dsy.py
@@ -28,21 +28,23 @@ def main():
     # read config file
     config_dict = {
         # 'reference database'
-        'ref_fasta': cf.get('Config', 'ref_fasta'),
-        'gtf_file': cf.get('Config', 'gtf_file'),
-
+        'tra_fastqs': cf.get('Config', 'tra_fastqs'),
+        'tra_fastq_rd1' : cf.get('Config', 'tra_fastqs').split()[0],
+        'trb_fastqs': cf.get('Config', 'trb_fastqs'),
+        'trb_fastq_rd1': cf.get('Config', 'trb_fastqs').split()[0],
+        'tra_barcode_file': cf.get('Config', 'tra_barcode_file'),
+        'trb_barcode_file': cf.get('Config', 'trb_barcode_file'),
+        
         # project name
         'project_name' : cf.get("Config", "project_name"),
-        'sample_name'  : cf.get("Config", "sample_name"),
-        'RNA_fastqs' : cf.get("Config", "RNA_fastqs"),
-        'scripts_dir': cf.get('Config', 'scripts_dir'),
-        # alignment
-        'STAR': cf.get('Config', "STAR"),
-        'STAR_index': cf.get('Config', "STAR_index"),
-        'samtools' : cf.get('Config', "samtools"),
+        'tra_sample_name'  : cf.get("Config", "tra_sample_name"),
+        'trb_sample_name'  : cf.get("Config", "trb_sample_name"),
+        'merged_sample_name' : cf.get("Config", "merged_sample_name"),
+        'expected_cell_number': cf.get("Config", "expected_cell_number"),
 
-        # expression
-        'featureCounts': cf.get('Config', "featureCounts"),
+        # alignment
+        'mixcr': cf.get('Config', "mixcr"),
+        'scripts_dir': cf.get('Config', "scripts_dir"),
     }
     #make directories:
     project_dir = os.path.abspath(".") + '/' + config_dict['project_name']
@@ -50,25 +52,56 @@ def main():
     print("# Create work directory")
 
     # generate shell
-    shell_name = project_dir + '/work.' + config_dict['project_name'] + '.RNASeq.sh'
+    shell_name = project_dir + '/work.' + config_dict['project_name'] + '.sh'
     # only open a file so use try:finally to close.
 
     with open(shell_name,"w") as f:
         # align
-        f.write("{Mixcr} -Xmx64G -Xms64G analyze shotgun --align -OsaveOriginalReads=true --species hs --starting-material rna --receptor-type tcr  -r {sample_name}_TRA.report  {tra_fastqs} {sample_name}_TRA.mixcr.out \n".format(**config_dict))
-        f.write("{Mixcr} -Xmx64G -Xms64G analyze shotgun --align -OsaveOriginalReads=true --species hs --starting-material rna --receptor-type tcr  -r {sample_name}_TRB.report  {tra_fastqs} {sample_name}_TRB.mixcr.out \n".format(**config_dict))
-        f.write("mkdir -p {sample_name}_TRA {sample_name}_TRB \n".format(**config_dict))
-        f.write("mixcr exportReadsForClones -s {sample_name}_TRA.mixcr.out.clna {sample_name}_TRA/{sample_name}_TRA.\n".format(**config_dict))
-        f.write("mixcr exportReadsForClones -s {sample_name}_TRB.mixcr.out.clna {sample_name}_TRB/{sample_name}_TRB.\n".format(**config_dict))
+        f.write("{mixcr} -Xmx64G -Xms64G analyze shotgun --align -OsaveOriginalReads=true --species hs --starting-material rna --receptor-type tcr  -r {tra_sample_name}.report  {tra_fastqs} {tra_sample_name}.mixcr.out \n".format(**config_dict))
+        f.write("{mixcr} -Xmx64G -Xms64G analyze shotgun --align -OsaveOriginalReads=true --species hs --starting-material rna --receptor-type tcr  -r {trb_sample_name}.report  {trb_fastqs} {trb_sample_name}.mixcr.out \n".format(**config_dict))
+        f.write("mkdir -p {tra_sample_name} {trb_sample_name} \n".format(**config_dict))
+        f.write("{mixcr} exportReadsForClones -s {tra_sample_name}.mixcr.out.clna {tra_sample_name}/{tra_sample_name}.\n".format(**config_dict))
+        f.write("{mixcr} exportReadsForClones -s {trb_sample_name}.mixcr.out.clna {trb_sample_name}/{trb_sample_name}.\n".format(**config_dict))
 
-        f.write("python3 {scripts_dir}/0.read2barcode.py {TRA_Barcode_file} ../data/{sample_name}_S1_L001_R1_001.fastq.gz {sample_name}_TRA\n".format(**config_dict))
-        f.write("python3 {scripts_dir}/0.read2barcode.py {TRB_Barcode_file} ../data/{sample_name}_S1_L001_R1_001.fastq.gz {sample_name}_TRB\n".format(**config_dict))
+        f.write("python3 {scripts_dir}/0.read2barcode.py {tra_barcode_file} {tra_fastq_rd1} {tra_sample_name}\n".format(**config_dict))
+        f.write("python3 {scripts_dir}/0.read2barcode.py {trb_barcode_file} {trb_fastq_rd1} {trb_sample_name}\n".format(**config_dict))
         
-        f.write("python3 {scripts_dir}/1.mixcr2barcode.py G125E2L1_TRA.barcode.read.txt G125E2L1_TRA.mixcr.out.clonotypes.TRA.txt TRA\n".format(**config_dict))
+        f.write("python3 {scripts_dir}/1.mixcr2barcode.py {tra_sample_name}.barcode.read.txt {tra_sample_name}.mixcr.out.clonotypes.TRA.txt TRA\n".format(**config_dict))
+        f.write("python3 {scripts_dir}/1.mixcr2barcode.py {trb_sample_name}.barcode.read.txt {trb_sample_name}.mixcr.out.clonotypes.TRB.txt TRB\n".format(**config_dict))
 
-        # add TPM and add RNAseq read depth.
-        f.write("python3 {scripts_dir}/add_TPM.py {sample_name}.variants.funcotated.with.minigene.MAF.xls {sample_name}.RNAseq.transcript.counts.TPM.txt transcript_id\n".format(**config_dict))
-        f.write("python3 {scripts_dir}/add_RNAseq_read_depth.py {sample_name}.variants.funcotated.with.minigene.MAF.add.TPM.xls {sample_name}.RNAseq.mpileup.txt\n".format(**config_dict))
+        ##########################
+        f.write("cat {tra_sample_name}.TRA.clone.reads.barcode.txt {trb_sample_name}.TRB.clone.reads.barcode.txt  >Total.{merged_sample_name}.TRAB.clone.reads.barcode.txt \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/2.transTalbe2matrix.py Total.{merged_sample_name}.TRAB.clone.reads.barcode.txt\n".format(**config_dict))
+        f.write("python3 {scripts_dir}/2.1.optional_filter_median.py Total.{merged_sample_name}.TRAB.clone.reads.barcode.txt \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/5.permutation_test.multi_process.py Total.{merged_sample_name}.TRAB.wells.boole.matrix.Filtered.csv  {merged_sample_name}.permutation_test.10000.out.Filtered.txt \n".format(**config_dict))
+        f.write("mkdir -p {merged_sample_name}_Shotgun/FromA2B {merged_sample_name}_Shotgun/FromB2A \n\n".format(**config_dict))
+        
+        ######################### FromA2B
+        f.write("python3 {scripts_dir}/3.calculate.p-value.FromA2B.py Total.{merged_sample_name}.TRAB.clone.reads.barcode.Filtered.txt 50 >{merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/3.calculate.p-value.FromA2B.py Total.{merged_sample_name}.TRAB.clone.reads.barcode.Filtered.txt 10 >{merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/6.permutation_true.v3.py {merged_sample_name}.permutation_test.10000.out.Filtered.txt {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.xls {expected_cell_number} > {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/6.permutation_true.v3.py {merged_sample_name}.permutation_test.10000.out.Filtered.txt {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.xls {expected_cell_number} > {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.xls |awk '$7>=5 && $10<0.1' >{merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.filtered.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.xls |awk '$7>=5 && $10<0.1' >{merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.filtered.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.xls |awk '$7>=3 && $10<0.1' >{merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.filtered.sharedwells.3.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.xls |awk '$7>=3 && $10<0.1' >{merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.filtered.sharedwells.3.xls \n\n".format(**config_dict))
+        
+        ########################## FromB2A
+        f.write("python3 {scripts_dir}/3.calculate.p-value.FromB2A.py Total.{merged_sample_name}.TRAB.clone.reads.barcode.Filtered.txt 50 >{merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/3.calculate.p-value.FromB2A.py Total.{merged_sample_name}.TRAB.clone.reads.barcode.Filtered.txt 10 >{merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/6.permutation_true.v3.py {merged_sample_name}.permutation_test.10000.out.Filtered.txt {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.xls {expected_cell_number} > {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/6.permutation_true.v3.py {merged_sample_name}.permutation_test.10000.out.Filtered.txt {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.xls {expected_cell_number} > {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.xls |awk '$7>=5 && $10<0.1' >{merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.filtered.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.xls |awk '$7>=5 && $10<0.1' >{merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.filtered.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.xls |awk '$7>=3 && $10<0.1' >{merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.filtered.sharedwells.3.xls \n".format(**config_dict))
+        f.write("less {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.xls |awk '$7>=3 && $10<0.1' >{merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.filtered.sharedwells.3.xls \n\n".format(**config_dict))
+        
+        ########################### sub
+        f.write("python3 {scripts_dir}/9.compare.A2B.v3.py {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.filtered.sharedwells.3.xls {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.filtered.sharedwells.3.xls > {merged_sample_name}_Shotgun/A2B_B2A_detail.threshold.10.sharedwells.3.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/9.compare.A2B.v3.py {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.filtered.sharedwells.3.xls {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.filtered.sharedwells.3.xls > {merged_sample_name}_Shotgun/A2B_B2A_detail.threshold.50.sharedwells.3.xls \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/10.merge.A2B.B2A.v1.py {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.10.add.filtered.sharedwells.3.xls {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.10.add.filtered.sharedwells.3.xls  {merged_sample_name}_Shotgun/A2B_B2A_detail.threshold.10.sharedwells.3.new.xlsx \n".format(**config_dict))
+        f.write("python3 {scripts_dir}/10.merge.A2B.B2A.v1.py {merged_sample_name}_Shotgun/FromA2B/Total.pairs.FromA2B.threshold.50.add.filtered.sharedwells.3.xls {merged_sample_name}_Shotgun/FromB2A/Total.pairs.FromB2A.threshold.50.add.filtered.sharedwells.3.xls  {merged_sample_name}_Shotgun/A2B_B2A_detail.threshold.50.sharedwells.3.new.xlsx \n".format(**config_dict))
+
     print("all finished!")
 
 
