adc84b96ff882047eabaf3523a534e72e5b7029c
levin <dushiyi319@163.com>
2020-09-14 09:05:54 +0800

    some changes

--- Shotgun_TCRseq/bin/2.2.draw.readCount.plot.R
+++ Shotgun_TCRseq/bin/2.2.draw.readCount.plot.R
@@ -2,7 +2,11 @@ library(ggplot2)
 args = commandArgs(T)
 
 for (each_file in args){
-    print(each_file)
+    print(paste("input:", each_file, sep=" "))
+    a = unlist(strsplit(each_file,"/"))
+    sample_name = unlist(strsplit(a[length(a)],"\\."))[2] # Total.G208E1.TRAB.wells.boole.matrix.csv : extract G208E1
+    # print(sample_name)
+
     data = read.table(each_file, sep = ",", header = T)
     tra_data = subset(data, clone_type == 'TRA')[,c(-1,-2)]
     trb_data = subset(data, clone_type == 'TRB')[,c(-1,-2)]
@@ -21,52 +25,63 @@ for (each_file in args){
     names(trb_data_sum_t) = c("data_sum","barcode","groups")
 
     total_data = rbind(tra_data_sum_t, trb_data_sum_t)
-    common_themes = theme(plot.title = element_text(hjust = 0.5),
+    common_themes = theme(plot.title = element_text(size = 17, hjust = 0.5),
                 panel.grid.major = element_line(colour = NA), # 去掉网格线
                 panel.background = element_blank(), # 去掉背景
                 panel.grid.major.x = element_line(),  # 横向网格线linetype=2,xuxian
                 panel.grid.major.y = element_line(), # 纵向网格线
                 panel.border = element_rect(fill=NA), # 边框
                 axis.line = element_line(), # 坐标轴线
-                legend.title = element_blank()
+		axis.title = element_text(size = 17),
+                legend.title = element_blank(),
+		legend.position = c(0.95,0.95), # bottom, right, 'left'
+		legend.background = element_rect(colour = NA, fill = NA),
           )
+    
+    if(length(unique(total_data$barcode))>24){
+        axis_text_x = theme(axis.text.x = element_text(vjust = 0.5,hjust = 0.5, angle = 90))
+    }else{
+        axis_text_x = theme(axis.text.x = element_text(vjust = 0.5,hjust = 0.5, angle = 0))
+    }
 
-    if (endsWith(each_file, ".wells.count.matrix.csv")){
+    if (endsWith(each_file, ".wells.count.matrix.Raw.csv")){
         p = ggplot(total_data, aes(x=barcode, y=data_sum, group=groups,color=groups)) +geom_point() + geom_line() + theme_bw() +
           xlab("Well Position") + ylab("Total Raw Read Count") +
           ggtitle("Total Raw read count for Each Well") +
-          common_themes +
-          scale_fill_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Raw", "TRB-Raw"))
-        ggsave( "read_count_for_each_well.Raw.jpg", width=10, height=10)
-        print("output : read_count_for_each_well.Raw.jpg")
-    }else if (endsWith(each_file, ".wells.boole.matrix.csv")){
+          common_themes + axis_text_x +
+          scale_colour_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Raw", "TRB-Raw"))
+        ggsave(paste(sample_name, "read_count_for_each_well.Raw.png", sep = "_"), width = 10, height = 6)
+        # png(paste(sample_name, "read_count_for_each_well.Raw.png", sep="_"))
+	# print(p)
+        print(paste("output: ", sample_name, "read_count_for_each_well.Raw.png",sep="_"))
+    }else if (endsWith(each_file, ".wells.boole.matrix.Raw.csv")){
         p = ggplot(total_data, aes(x=barcode,y=data_sum, group=groups, color=groups)) + geom_point() + geom_line() + theme_bw() +
             xlab("Well Position") + ylab("TCR Raw Count") +
             ggtitle("Number of TCRs detected for each well") +
             common_themes +
-            scale_y_continuous(breaks = seq(0,100,5)) +
-            scale_fill_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Raw", "TRB-Raw"))
-        ggsave( "TCRs_count_for_each_well.Raw.jpg", width=10, height=10)
-        print("TCRs_count_for_each_well.Raw.jpg")
+            axis_text_x +
+            scale_colour_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Raw", "TRB-Raw"))
+        ggsave(paste(sample_name, "TCRs_count_for_each_well.Raw.png", sep="_"), width = 10, height = 6)
+        print(paste("output: ", sample_name, "TCRs_count_for_each_well.Raw.png",sep="_"))
     }else if (endsWith(each_file, ".wells.count.matrix.Filtered.csv")){
         p = ggplot(total_data, aes(x=barcode, y=data_sum, group=groups,color=groups)) + geom_point() + geom_line() + theme_bw() +
           xlab("Well Position") + ylab("Total Filtered Read Count") +
           ggtitle("Total Filtered read count for Each Well") +
-          common_themes +
-          scale_fill_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Filtered", "TRB-Filtered"))
-        ggsave( "read_count_for_each_well.Filtered.jpg", width=10, height=10)
-        print("read_count_for_each_well.Filtered.jpg")
+          common_themes + axis_text_x +
+          scale_colour_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Filtered", "TRB-Filtered"))
+        ggsave(paste(sample_name, "read_count_for_each_well.Filtered.png", sep="_"), width = 10, height = 6)
+        print(paste("output: ", sample_name, "read_count_for_each_well.Filtered.png", sep="_"))
     }else if (endsWith(each_file, ".wells.boole.matrix.Filtered.csv")){
         p = ggplot(total_data, aes(x=barcode, y=data_sum, group=groups, color=groups)) + geom_point() + geom_line() + theme_bw() +
           xlab("Well Position") + ylab("TCR Filtered Count") +
           ggtitle("Number of TCRs detected for each well") +
-          common_themes +
-          scale_y_continuous(breaks = seq(0,100,5)) +
-          scale_fill_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Filtered", "TRB-Filtered"))
-        ggsave( "TCRs_count_for_each_well.Filtered.jpg", width=10, height=10)
-        print("TCRs_count_for_each_well.Filtered.jpg")
+          common_themes + axis_text_x +
+          scale_colour_discrete(breaks=c("TRA","TRB"), labels=c("TRA-Filtered", "TRB-Filtered"))
+        # ggsave(paste(sample_name, "TCRs_count_for_each_well.Filtered.png",sep="_"), width=10, height=10)
+        ggsave(paste(sample_name, "TCRs_count_for_each_well.Filtered.png",sep="_"), width = 10, height = 6)
+        print(paste("output: ", sample_name, "TCRs_count_for_each_well.Filtered.png",sep="_"))
     }else{
-        exit("your input file name are not correct.\nError: Incorrect File Name.\n")
+        print(paste("your input file name are not correct.\nError: Incorrect File Name.\n",sample_name,sep=":"))
     }
 }
 
