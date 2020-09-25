d8d3bea1fd13f9524bac01932c8a001527536f1c
levin <dushiyi319@163.com>
2020-09-25 09:56:30 +0800

    some changes

--- 10xGenomics/scripts/statistic_summary_report.py
+++ 10xGenomics/scripts/statistic_summary_report.py
@@ -1,6 +1,13 @@
 import sys
 import pandas as pd
 
+
+def usage():
+    """
+    python3 statistic_summary_report.py /cygene2/work/P0027-OTS-CancerPeptide-Shotgun20200527/2ndExp/10xscTCRseq/analysis/*/outs/metrics_summary.csv 
+    """
+
+
 print("Sample.ID\tDetected.Cell.Num\tDetected.TCRpair.Num\tFraction.Reads.in.Cells\tMedian.TRA.UMIs.per.Cell\tMedian.TRB.UMIs.per.Cell")
 for each in sys.argv[1:]:
     name = each.split("/")[-3]
