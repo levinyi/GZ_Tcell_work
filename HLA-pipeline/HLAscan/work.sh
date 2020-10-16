7593c43ff98c9ac716ec18897f65df217ba05f0b
levin <dushiyi319@163.com>
2020-10-17 07:46:32 +0800

    add a file

--- HLA-pipeline/HLAscan/work.sh
+++ HLA-pipeline/HLAscan/work.sh
@@ -1,2 +1,4 @@
 # hla_scan -b NA12155.chr6.bam -d HLA-ALL.IMGT -v 38
 hla_scan -t 60 -l CP60005015-C01-WES_R1.fastq -r CP60005015-C01-WES_R2.fastq -d /cygene/database/imgt-hla/db/HLA-ALL.IMGT -g HLA-A HLA-B HLA-C 
+
+#time-consummer
