fa7d35dc1243bea706b9190958f0e4b5b606f595
levin <dushiyi319@163.com>
2020-10-23 09:20:37 +0800

    add a new project

---
+++ HLA-pipeline/mhc-prg-test/work.sh
@@ -0,0 +1,4 @@
+HLAtypeinference.pl --actions pnai --sampleIDs CP60005015_Normal --BAMs CP60005015-C01.Normal.sortedByCoord.bam --referenceGenome /cygene/database/GATK_resource_bundle/hg38/Homo_sapiens_assembly38.fasta
+
+
+abandon.
