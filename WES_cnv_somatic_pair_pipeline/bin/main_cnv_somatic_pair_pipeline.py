a03d3d5f5e268b96f38a2fc5d40cfb3b03efe443
levin <dushiyi319@163.com>
2020-12-18 12:09:13 +0800

    some changes

--- WES_cnv_somatic_pair_pipeline/bin/main_cnv_somatic_pair_pipeline.py
+++ WES_cnv_somatic_pair_pipeline/bin/main_cnv_somatic_pair_pipeline.py
@@ -37,7 +37,7 @@ def main():
 
         # Funcotator
         'funcotator_dataSources' : cf.get("GATK", "funcotator_dataSources"),
-        'ref_version' : cf.get("GATK", "ref_version"),
+        'funcotator_ref_version' : cf.get("GATK", "funcotator_ref_version"),
         
         # 'python' : cf.get('Config', 'python'),
         'scripts_dir': cf.get('Config', 'scripts_dir'),
@@ -67,17 +67,18 @@ def main():
     with open(shell_name,"w") as f:
         ## CNVTasks.PreprocessIntervals
         f.write("""{gatk} --java-options \"-Xms{java_mem}G\" \\
-            PreprocessIntervals \\
+        PreprocessIntervals \\
             -L {intervals} \\
             --reference {ref_fasta} \\
             --padding 250 \\
             --bin-length 0 \\
             --interval-merging-rule OVERLAPPING_ONLY \\
-            --output targets.preprocessed.interval_list \n""".format(**config_dict))
+            --output targets.preprocessed.interval_list \n\n""".format(**config_dict))
+
         # CNVTasks.CollectCounts as CollectCountsTumor
         # CNVTasks.CollectCounts as CollectCountsNormal
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            CollectReadCounts \\
+        CollectReadCounts \\
             -L targets.preprocessed.interval_list \\
             --reference {ref_fasta} \\
             --input  {tumor_bam} \\
@@ -85,137 +86,123 @@ def main():
             --interval-merging-rule OVERLAPPING_ONLY \\
             --output {sample_name}.Tumor.counts.hdf5 \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            CollectReadCounts \\
+        CollectReadCounts \\
             -L targets.preprocessed.interval_list \\
             --reference {ref_fasta} \\
             --input  {normal_bam} \\
             --format HDF5 \\
             --interval-merging-rule OVERLAPPING_ONLY \\
-            --output {sample_name}.Normal.counts.hdf5 \n""".format(**config_dict))
+            --output {sample_name}.Normal.counts.hdf5 \n\n""".format(**config_dict))
+
         # CNVTasks.CollectAllelicCounts as CollectAllelicCountsTumor
         # CNVTasks.CollectAllelicCounts as CollectAllelicCountsNormal
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            CollectAllelicCounts \\
+        CollectAllelicCounts \\
             -I {tumor_bam}  \\
             -R {ref_fasta} \\
             -L {common_sites} \\
             --minimum-base-quality 20 \\
             -O {sample_name}.Tumor.allelicCounts.tsv \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            CollectAllelicCounts \\
+        CollectAllelicCounts \\
             -I {normal_bam} \\
             -R {ref_fasta} \\
             -L {common_sites} \\
             --minimum-base-quality 20 \\
             -O {sample_name}.Normal.allelicCounts.tsv \n""".format(**config_dict))
+        f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
+        CreateReadCountPanelOfNormals \\
+            -I {sample_name}.Normal.counts.hdf5  \\
+            -O cnv.pon.hdf5 \n""".format(**config_dict))
         # DenoiseReadCounts as DenoiseReadCountsTumor
         # DenoiseReadCounts as DenoiseReadCountsNormal
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            DenoiseReadCounts \\
-            -I {sample_name}.Tumor.readcounts  \\
-            --count-panel-of-normals ~{read_count_pon} \\
-            --standardized-copy-ratios ~{sample_name}.Tumor.standardizedCR.tsv \\
-            --minimum-base-quality 20 \\
-            --denoised-copy-ratios ~{sample_name}.Tumor.denoisedCR.tsv \n""".format(**config_dict))
+        DenoiseReadCounts \\
+            -I {sample_name}.Tumor.counts.hdf5  \\
+            --count-panel-of-normals cnv.pon.hdf5 \\
+            --standardized-copy-ratios {sample_name}.Tumor.standardizedCR.tsv \\
+            --denoised-copy-ratios {sample_name}.Tumor.denoisedCR.tsv \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            DenoiseReadCounts \\
-            -I {sample_name}.Normal.readcounts  \\
-            --count-panel-of-normals ~{read_count_pon} \\
-            --standardized-copy-ratios ~{sample_name}.Normal.standardizedCR.tsv \\
-            --minimum-base-quality 20 \\
-            --denoised-copy-ratios ~{sample_name}.Normal.denoisedCR.tsv \n""".format(**config_dict))
+        DenoiseReadCounts \\
+            -I {sample_name}.Normal.counts.hdf5  \\
+            --count-panel-of-normals cnv.pon.hdf5 \\
+            --standardized-copy-ratios {sample_name}.Normal.standardizedCR.tsv \\
+            --denoised-copy-ratios {sample_name}.Normal.denoisedCR.tsv \n\n""".format(**config_dict))
+
         # ModelSegments as ModelSegmentsTumor
         # ModelSegments as ModelSegmentsNormal
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            ModelSegments \\
-            --denoised-copy-ratios ~Tumor.denoised_copy_ratios \\
-            --allelic-counts ~Tumor.allelic_counts \\
-            --normal-allelic-counts  ollectAllelicCountsNormal.allelic_counts \\
-            --minimum-total-allele-count-case ~min_total_allele_count_ \\
-            --window-size 0 \\
-            --output ~output_dir_Tumor \\
-            --output-prefix ~Tumor.entity_id \n""".format(**config_dict))
+        ModelSegments \\
+            --denoised-copy-ratios {sample_name}.Tumor.denoisedCR.tsv \\
+            --allelic-counts {sample_name}.Tumor.allelicCounts.tsv \\
+            --normal-allelic-counts {sample_name}.Normal.allelicCounts.tsv \\
+            --output ./ \\
+            --output-prefix {sample_name}.Tumor \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            ModelSegments \\
-            --denoised-copy-ratios ~Normal.denoised_copy_ratios \\
-            --allelic-counts ~Normal.allelic_counts \\
-            --minimum-total-allele-count-case ~min_total_allele_count_ \\
-            --window-size 0 \\
-            --output ~output_dir_Tumor \\
-            --output-prefix ~Normal.entity_id \n""".format(**config_dict))
+        ModelSegments \\
+            --denoised-copy-ratios {sample_name}.Normal.denoisedCR.tsv \\
+            --allelic-counts {sample_name}.Normal.allelicCounts.tsv \\
+            --output ./ \\
+            --output-prefix {sample_name}.Normal \n\n""".format(**config_dict))
         
         # CallCopyRatioSegments as CallCopyRatioSegmentsTumor
         # CallCopyRatioSegments as CallCopyRatioSegmentsNormal
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            CallCopyRatioSegments \\
-            --input ~Tumor.copy_ratio_segments \\
-            --output ~entity_id.called.seg \n""".format(**config_dict))
+        CallCopyRatioSegments \\
+            --input {sample_name}.Tumor.cr.seg \\
+            --output {sample_name}.Tumor.called.seg \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            CallCopyRatioSegments \\
-            --input ~Normal.copy_ratio_segments \\
-            --output ~Normal.entity_id.called.seg \n""".format(**config_dict))
+        CallCopyRatioSegments \\
+            --input {sample_name}.Normal.cr.seg \\
+            --output {sample_name}.Normal.called.seg \n\n""".format(**config_dict))
         
         # PlotDenoisedCopyRatios as PlotDenoisedCopyRatiosTumor
         # PlotDenoisedCopyRatios as PlotDenoisedCopyRatiosNormal
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            PlotDenoisedCopyRatios \\
-            --standardized-copy-ratios ~DenoiseReadCountsTumor.standardized_copy_ratios \\
-            --denoised-copy-ratios ~DenoiseReadCountsTumor.denoised_copy_ratios \\
+        PlotDenoisedCopyRatios \\
+            --standardized-copy-ratios {sample_name}.Tumor.standardizedCR.tsv  \\
+            --denoised-copy-ratios {sample_name}.Tumor.denoisedCR.tsv  \\
             --sequence-dictionary {ref_fasta}.dict \\
             --output ./ \\
-            --output-prefix CollectCountsTumor.entity_id \n""".format(**config_dict))
+            --output-prefix {sample_name}.Tumor \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
-            PlotDenoisedCopyRatios \\
-            --standardized-copy-ratios DenoiseReadCountsNormal.standardized_copy_ratios \\
-            --denoised-copy-ratios DenoiseReadCountsNormal.denoised_copy_ratios \\
+        PlotDenoisedCopyRatios \\
+            --standardized-copy-ratios {sample_name}.Normal.standardizedCR.tsv \\
+            --denoised-copy-ratios {sample_name}.Normal.denoisedCR.tsv \\
             --sequence-dictionary {ref_fasta}.dict \\
             --output ./ \\
-            --output-prefix ~CollectCountsNormal.entity_id \n""".format(**config_dict))
-        
+            --output-prefix {sample_name}.Normal \n\n""".format(**config_dict))
+
         # PlotModeledSegments as PlotModeledSegmentsTumor
         # PlotModeledSegments as PlotModeledSegmentsNormal
-        f.write("""{gatk} --java-options \"-Xmx~{java_mem}G\" \\
-            PlotModeledSegments \\
-            --denoised-copy-ratios DenoiseReadCountsTumor.denoised_copy_ratios \\
-            --allelic-counts ModelSegmentsTumor.het_allelic_counts \\
-            --segments ModelSegmentsTumor.modeled_segments \\
+        f.write("""{gatk} \\
+        PlotModeledSegments \\
+            --denoised-copy-ratios {sample_name}.Tumor.denoisedCR.tsv \\
+            --allelic-counts {sample_name}.Tumor.hets.tsv \\
+            --segments {sample_name}.Tumor.modelFinal.seg \\
             --sequence-dictionary {ref_fasta}.dict \\
             --output ./ \\
-            --output-prefix ~CollectCountsTumor.entity_id \n""".format(**config_dict))
+            --output-prefix {sample_name}.Tumor \n""".format(**config_dict))
         f.write("""{gatk} --java-options \"-Xmx{java_mem}G\" \\
-            PlotModeledSegments \\
-            --denoised-copy-ratios ~DenoiseReadCountsNormal.denoised_copy_ratios \\
-            --allelic-counts ~ModelSegmentsNormal.het_allelic_counts \\
-            --segments ~ModelSegmentsNormal.modeled_segments \\
+        PlotModeledSegments \\
+            --denoised-copy-ratios {sample_name}.Normal.denoisedCR.tsv \\
+            --allelic-counts {sample_name}.Normal.hets.tsv \\
+            --segments {sample_name}.Normal.modelFinal.seg \\
             --sequence-dictionary {ref_fasta}.dict \\
             --output ./ \\
-            --output-prefix CollectCountsNormal.entity_id \n""".format(**config_dict))
-
-        # # optional
-        # CNVOncotator.CNVOncotatorWorkflow as CNVOncotatorWorkflow
+            --output-prefix {sample_name}.Normal \n\n""".format(**config_dict))
 
         # CNVFuncotateSegments.CNVFuncotateSegmentsWorkflow as CNVFuncotateSegmentsWorkflow
         # https://github.com/broadinstitute/gatk/blob/master/scripts/cnv_wdl/somatic/cnv_somatic_funcotate_seg_workflow.wdl
-        '''
-        f.write(""""gatk --java-options "-Xmx~{command_mem_mb}m" FuncotateSegments \\
-             --data-sources-path $DATA_SOURCES_FOLDER \\
-             --ref-version ~{funcotator_ref_version} \\
-             --output-file-format SEG \\
-             -R ~{ref_fasta} \\
-             --segments ~{input_seg_file} \\
-             -O ~{basename_input_seg_file}.funcotated.tsv \\
-             ~{interval_list_arg} ~{default="" interval_list} \\
-             ~{"--transcript-selection-mode " + transcript_selection_mode} \\
-             ~{transcript_selection_arg}~{default="" sep=" --transcript-list " transcript_selection_list} \\
-             ~{annotation_def_arg}~{default="" sep=" --annotation-default " annotation_defaults} \\
-             ~{annotation_over_arg}~{default="" sep=" --annotation-override " annotation_overrides} \\
-             ~{excluded_fields_args}~{default="" sep=" --exclude-field " funcotator_excluded_fields} \\
-             ~{extra_args_arg} \n""".format(**config_dict))
-        '''
+        f.write("""{gatk} --java-options \"-Xmx{java_mem}G\"  \\
+        FuncotateSegments \\
+            --data-sources-path {funcotator_dataSources} \\
+            --ref-version {funcotator_ref_version} \\
+            --output-file-format SEG \\
+            -R {ref_fasta} \\
+            --segments {sample_name}.Normal.modelFinal.seg \\
+            -O {sample_name}.Normal.modelFinal.seg.funcotated.tsv \n\n""".format(**config_dict))
     print("all finished!")
 
 if __name__ == '__main__':
     main()
-
-
-
