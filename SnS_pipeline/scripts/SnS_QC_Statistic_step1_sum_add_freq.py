435218db5a3e069aaa82323127aa0a8bc03ec867
levin <dushiyi319@163.com>
2021-01-26 11:34:35 +0800

    some changes

--- SnS_pipeline/scripts/SnS_QC_Statistic_step1_sum_add_freq.py
+++ SnS_pipeline/scripts/SnS_QC_Statistic_step1_sum_add_freq.py
@@ -15,7 +15,8 @@ with open(freq_file, "r") as f:
         adict[a[0]] = a[4]
 
 
-pattern = re.compile(r'.*\.(clonotype.*)\.[ATCG]{8}.*')
+# pattern = re.compile(r'.*\.(clonotype.*)\.[ATCG]{8}.*')
+# pattern = re.compile(r'.*\.(.*)\.[ATCG]{12}.*')
 with open(sum_file, "r") as f:
     print("Molecule\tCDR3aJ.Identity\tCDR3aJ.Coverage%\tCDR3aJ.Mutation\tCDR3aJ.InDel\tconA.Identity\tconA.Coverage%\tconA.Mutation\tconA.InDel\tTRAV.Identity\tTRAV.Coverage%\tTRAV.Mutation\tTRAV.InDel\tCDR3bJ.Identity\tCDR3bJ.Coverage%\tCDR3bJ.Mutation\tCDR3bJ.InDel\tconB.Identity\tconB.Coverage%\tconB.Mutation\tconB.InDel\tTRBV.Identity\tTRBV.Coverage%\tTRBV.Mutation\tTRBV.InDel\tSum.of.5\tTCR_freq")
     for line in f:
@@ -24,9 +25,11 @@ with open(sum_file, "r") as f:
         line = line.rstrip("\n")
         c = line.split()
         sum5 = int(c[3])+int(c[4])+int(c[7])+int(c[8])+int(c[11])+int(c[12])+int(c[15])+int(c[16])+int(c[19])+int(c[20])
-        match = pattern.match(c[0])
+        # match = pattern.match(c[0])
         # print(match.group(1))
-        tcr_freq = adict[match.group(1)]
+        # tcr_freq = adict[match.group(1)]
         # print(tcr_freq)
+        match = ".".join(c[0].split(".")[1:-2])
+        tcr_freq = adict[match]
 
         print("{}\t{}\t{}".format("\t".join(c),sum5,tcr_freq))
