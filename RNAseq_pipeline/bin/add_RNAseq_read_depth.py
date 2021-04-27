688e92eab120d210444a36bb31a42b0a9f38fdb9
levin <dushiyi319@163.com>
2021-04-27 13:02:39 +0800

    some changes

--- RNAseq_pipeline/bin/add_RNAseq_read_depth.py
+++ RNAseq_pipeline/bin/add_RNAseq_read_depth.py
@@ -5,6 +5,14 @@ import re
 import pandas as pd
 
 
+def usage():
+    print("""
+        python {} <xls> <tpm file>
+    Update:
+        20210410    add parmaters.
+        20200703    Created.
+    """.format(os.path.basename(sys.argv[0])))
+
 def addtwodimdict(thedict, key_a, key_b, val):
     ''' this is a function to add two dimetion dict '''
     if key_a in thedict:
@@ -20,15 +28,32 @@ def deal_mpileup(RNAseq_mpileup_file):
         for line in f:
             line = line.rstrip("\n")
             chrom, pos, mapped_base, reads_count, mapping_info, mapping_qul = line.split("\t")
-            match = re.search(r'[ATCG]',mapping_info.upper())
-            if match:
-                match.group()
+            match_mut = re.search(r'[ATCG]', mapping_info.upper())
+            '''
+            if match_mut:
+                print(match_mut.group())
                 mutatype = mapping_info.upper().count(match.group())
             else:
                 mutatype = 0
-            wildtype = int(reads_count) - mutatype 
+            '''
+            wildtype = mapping_info.upper().count(".") + mapping_info.upper().count(",") + mapping_info.upper().count("$")
+            if '+' in mapping_info:
+                ins_type = mapping_info.upper().count("+")
+            else:
+                ins_type = 0
+            if '*' in mapping_info:
+                del_type = mapping_info.upper().count("*")
+            else:
+                del_type = 0
+            if match_mut:
+                mutatype = mapping_info.upper().count(match_mut.group())
+            else:
+                mutatype = 0
+            #wildtype = int(reads_count) - mutatype
             addtwodimdict(a_dict, pos, 'W', wildtype)
             addtwodimdict(a_dict, pos, 'M', mutatype)
+            addtwodimdict(a_dict, pos, 'I', ins_type)
+            addtwodimdict(a_dict, pos, 'D', del_type)
     return a_dict
 
 xls_file = sys.argv[1]
@@ -52,10 +77,17 @@ output_file.write("{}\n".format("\t".join(contain_fields)))
 data = pd.read_table(xls_file, sep="\t")
 for index, row in data.iterrows():
     Position_start = str(row["Start_Position"])
+    Variant_Type = str(row['Variant_Type'])
     if Position_start in mpileup_dict:
         RNA_wild_reads = int(mpileup_dict[Position_start].get("W", 0))
-        RNA_muta_reads = int(mpileup_dict[Position_start].get("M", 0))
+        if Variant_Type == 'SNP':
+            RNA_muta_reads = int(mpileup_dict[Position_start].get("M", 0))
+        elif Variant_Type == 'DEL':
+            RNA_muta_reads = int(mpileup_dict[Position_start].get("D", 0))
+        elif Variant_Type == 'INS':
+            RNA_muta_reads = int(mpileup_dict[Position_start].get("I", 0))
     else:
+        print("position start not in mpileup dict. please check!")
         RNA_wild_reads = 0
         RNA_muta_reads = 0
     output_file.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
@@ -64,6 +96,6 @@ for index, row in data.iterrows():
         row["Tumor_Seq_Allele1"], row["Tumor_Seq_Allele2"], row["dbSNP_RS"], row["Genome_Change"], row["Annotation_Transcript"],
         row["Transcript_Strand"], row["Transcript_Exon"], row["cDNA_Change"], row["Codon_Change"], row["Protein_Change"],
         row["Refseq_mRNA_Id"], row["tumor_f"], row["t_alt_count"], row["t_ref_count"], row["n_alt_count"], 
-        row["n_ref_count"], row["DP"],row["Mutated_Minigene"],row["Wild-Type_Minigene"],RNA_muta_reads,RNA_wild_reads, 
+        row["n_ref_count"], row["DP"],row["Mutated_Minigene"],row["Wild-Type_Minigene"],RNA_muta_reads, RNA_wild_reads, 
         row["TPM"],
         ))
