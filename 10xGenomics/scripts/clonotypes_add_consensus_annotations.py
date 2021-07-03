1aecbf8a0add62d8b6425fe956c8c5952277b871
levin <dushiyi319@163.com>
2021-07-03 08:43:47 +0800

    some changes

--- 10xGenomics/scripts/clonotypes_add_consensus_annotations.py
+++ 10xGenomics/scripts/clonotypes_add_consensus_annotations.py
@@ -25,7 +25,7 @@ def read_clonotypes(file1):
             line = line.rstrip("\n")
             if line.startswith("clonotype_id"):
                 continue
-            clonotype_id,frequency, proportion,cdr3s_aa,cdr3s_nt = line.split(",")
+            clonotype_id,frequency, proportion,cdr3s_aa,cdr3s_nt,inkt_evidence,mait_evidence = line.split(",")
             adict.setdefault(clonotype_id,{})['freq'] = frequency
             adict.setdefault(clonotype_id,{})['prop'] = proportion
     return adict
@@ -44,24 +44,27 @@ def two_dim_dict(thedict, key_a, key_b, value):
 
 
 def read_consensus(file2):
-	len_dict = {}
-	detail_dict = {}
-	with open(file2, "r") as f2:
-		for line in f2:
-			line = line.rstrip("\n")
-			if line.startswith("clonotype_id"):
-				continue
-			clonotype_id,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,cdr3,cdr3_nt,reads,umis = line.split(",")
-			two_dim_dict(len_dict,clonotype_id,chain,1)
-			if clonotype_id in detail_dict:
-				if chain in detail_dict[clonotype_id]:
-					new_chain = chain + "_1"
-					detail_dict.setdefault(clonotype_id,{})[new_chain] = [consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,cdr3,cdr3_nt,reads,umis]
-				else:
-					detail_dict.setdefault(clonotype_id,{})[chain] = [consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,cdr3,cdr3_nt,reads,umis]
-			else:
-				detail_dict.setdefault(clonotype_id,{})[chain] = [consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,cdr3,cdr3_nt,reads,umis]
-	return len_dict, detail_dict
+    len_dict = {}
+    detail_dict = {}
+    with open(file2, "r") as f2:
+        for line in f2:
+            line = line.rstrip("\n")
+            if line.startswith("clonotype_id"):
+                continue
+            a = line.split(",")
+            clonotype_id = a[0]
+            chain = a[3]
+
+            two_dim_dict(len_dict,clonotype_id,chain,1)
+            if clonotype_id in detail_dict:
+                if chain in detail_dict[clonotype_id]:
+                    new_chain = chain + "_1"
+                    detail_dict.setdefault(clonotype_id,{})[new_chain] = a[1:]
+                else:
+                    detail_dict.setdefault(clonotype_id,{})[chain] = a[1:]
+            else:
+                detail_dict.setdefault(clonotype_id,{})[chain] = a[1:]
+    return len_dict, detail_dict
 
 
 def main():
@@ -80,7 +83,7 @@ def main():
         clonotype_dict = read_clonotypes(file1)
         len_dict, detail_dict = read_consensus(file2)
 	# print(json.dumps(detail_dict, indent=4))
-        print("clonotype_id,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,cdr3,cdr3_nt,reads,umis,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,cdr3,cdr3_nt,reads,umis,frequency, proportion")
+        print("clonotype_id,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,fwr1,fwr1_nt,cdr1,cdr1_nt,fwr2,fwr2_nt,cdr2,cdr2_nt,fwr3,fwr3_nt,cdr3,cdr3_nt,fwr4,fwr4_nt,reads,umis,v_start,v_end,v_end_ref,j_start,j_start_ref,j_end,fwr1_start,fwr1_end,cdr1_start,cdr1_end,fwr2_start,fwr2_end,cdr2_start,cdr2_end,fwr3_start,fwr3_end,cdr3_start,cdr3_end,fwr4_start,fwr4_end,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,fwr1,fwr1_nt,cdr1,cdr1_nt,fwr2,fwr2_nt,cdr2,cdr2_nt,fwr3,fwr3_nt,cdr3,cdr3_nt,fwr4,fwr4_nt,reads,umis,v_start,v_end,v_end_ref,j_start,j_start_ref,j_end,fwr1_start,fwr1_end,cdr1_start,cdr1_end,fwr2_start,fwr2_end,cdr2_start,cdr2_end,fwr3_start,fwr3_end,cdr3_start,cdr3_end,fwr4_start,fwr4_end,frequency, proportion")
         for each in len_dict:
             if len(len_dict[each]) == 2:
                 if len_dict[each]['TRA'] == 1 and len_dict[each]['TRB'] == 1:
