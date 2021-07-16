1f68e1a0e20b5b4b3ebf39f3963be480f4c7f784
levin <dushiyi319@163.com>
2021-07-16 09:36:58 +0800

    some changes

--- RNAseq_pipeline/bin/merge.tpm.py
+++ RNAseq_pipeline/bin/merge.tpm.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pandas as pd
 
 def usage():
     '''
@@ -9,37 +10,24 @@ def usage():
     python merge.tpm.py /cygene2/work/Tumor_Expression_Data/*.TPM.txt >test.txt
 
     update:
+    20210713. fix bug: GB001003-Tumor-pc-P6 key error.
     20210204. added usage description.
     2020xxxx. created.
     '''
     pass
 
-def addtwodimdict(thedict, key_a, key_b, val):
-    ''' this is a function to add two dimetion dict '''
-    if key_a in thedict:
-        thedict[key_a].update({key_b: val})
-    else:
-        thedict.update({key_a: {key_b: val}})
-    return thedict
 
-gene_dict = {}
-gene_list = []
-sample_list = []
-for each in sys.argv[1:]:
-    sample_name = os.path.basename(each).split(".")[0]
-    sample_list.append(sample_name)
-    with open(each, "r") as f:
-        for line in f:
-            line = line.rstrip("\n")
-            gene,tpm = line.split()
-            addtwodimdict(gene_dict,gene,sample_name,tpm)
-            gene_list.append(gene)
+def main():
+    big_DataFrame = pd.DataFrame()
+    for each in sys.argv[1:]:
+        sample_name = os.path.basename(each).split(".")[0]
+        df1 = pd.read_table(each, sep="\t", header=None, names=["Gene",sample_name])
+        if big_DataFrame.empty :
+            big_DataFrame = df1
+        else:
+            big_DataFrame = pd.merge(big_DataFrame, df1, on='Gene', how='outer')
+    print(big_DataFrame.to_csv(sep="\t", index=False))
 
-gene_list = set(gene_list)
-print("Geneid\t{}".format("\t".join(sample_list)))
-for gene in gene_list:
-    gene_tpm = []
-    for sample in sample_list:
-        v = gene_dict[gene][sample]
-        gene_tpm.append(v)
-    print("{}\t{}".format(gene, "\t".join(gene_tpm)))
+
+if __name__ == '__main__':
+    main()
