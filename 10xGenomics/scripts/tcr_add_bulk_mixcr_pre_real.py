3c85069eb1559a21525998dc942bf282b1f3ca97
levin <dushiyi319@163.com>
2021-08-13 17:29:47 +0800

    some changes

--- 10xGenomics/scripts/tcr_add_bulk_mixcr_pre_real.py
+++ 10xGenomics/scripts/tcr_add_bulk_mixcr_pre_real.py
@@ -1,4 +1,5 @@
 import sys
+import pandas as pd
 
 def get_tcr_dict(afile):
     adict = {}
@@ -14,23 +15,26 @@ def get_tcr_dict(afile):
 
 
 
+def main():
+    tcr_file = sys.argv[1]
+    pre_real_tra_file = sys.argv[2]
+    pre_real_trb_file = sys.argv[3]
 
-tcr_file = sys.argv[1]
-pre_real_tra_file = sys.argv[2]
-pre_real_trb_file = sys.argv[3]
-
-tra_dict, tra_header = get_tcr_dict(pre_real_tra_file)
-trb_dict, trb_header = get_tcr_dict(pre_real_trb_file)
+    tra_dict, tra_header = get_tcr_dict(pre_real_tra_file)
+    trb_dict, trb_header = get_tcr_dict(pre_real_trb_file)
 
 
-with open(tcr_file, "r") as f:
-    for line in f:
-        line = line.rstrip("\n")
-        if line.startswith("clonotype_id"):
-            print("{},{},{}".format(line,",".join(tra_header),",".join(trb_header)))
-            continue
-        c = line.split(",")
-        TRA_clonotype = c[4].split("*")[0].replace("/","") + c[10] + c[6].split("*")[0].replace("/","")
-        TRB_clonotype = c[17].split("*")[0].replace("/","") + c[23] + c[19].split("*")[0].replace("/","")
-        #print(TRA_clonotype, TRB_clonotype)
-        print("{},{},{}".format(",".join(c),tra_dict.get(TRA_clonotype,",".join(['0']*13)),trb_dict.get(TRB_clonotype, ",".join(['0']*13))))
+    with open(tcr_file, "r") as f:
+        for line in f:
+            line = line.rstrip("\n")
+            if line.startswith("clonotype_id"):
+                print("{},{},{}".format(line,",".join(tra_header),",".join(trb_header)))
+                continue
+            c = line.split(",")
+            TRA_clonotype = c[4].split("*")[0].replace("/","") + c[10] + c[6].split("*")[0].replace("/","")
+            TRB_clonotype = c[17].split("*")[0].replace("/","") + c[23] + c[19].split("*")[0].replace("/","")
+            #print(TRA_clonotype, TRB_clonotype)
+            print("{},{},{}".format(",".join(c),tra_dict.get(TRA_clonotype,",".join(['0']*13)),trb_dict.get(TRB_clonotype, ",".join(['0']*13))))
+
+if __name__ == '__main__':
+    main()
