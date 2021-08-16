293bf230434a17b5d0cc0db75242856e117540b6
levin <dushiyi319@163.com>
2021-08-16 12:01:28 +0800

    some changes

--- 10xGenomics/scripts/tcr_add_bulk_mixcr_pre_real.py
+++ 10xGenomics/scripts/tcr_add_bulk_mixcr_pre_real.py
@@ -23,7 +23,7 @@ def main():
     tra_dict, tra_header = get_tcr_dict(pre_real_tra_file)
     trb_dict, trb_header = get_tcr_dict(pre_real_trb_file)
 
-
+    print(tcr_file)
     with open(tcr_file, "r") as f:
         for line in f:
             line = line.rstrip("\n")
@@ -33,8 +33,8 @@ def main():
             c = line.split(",")
             TRA_clonotype = c[4].split("*")[0].replace("/","") + c[10] + c[6].split("*")[0].replace("/","")
             TRB_clonotype = c[17].split("*")[0].replace("/","") + c[23] + c[19].split("*")[0].replace("/","")
-            #print(TRA_clonotype, TRB_clonotype)
-            print("{},{},{}".format(",".join(c),tra_dict.get(TRA_clonotype,",".join(['0']*13)),trb_dict.get(TRB_clonotype, ",".join(['0']*13))))
+            ## print(TRA_clonotype, TRB_clonotype)
+            # print("{},{},{}".format(",".join(c),tra_dict.get(TRA_clonotype,",".join(['0']*13)),trb_dict.get(TRB_clonotype, ",".join(['0']*13))))
 
 if __name__ == '__main__':
     main()
