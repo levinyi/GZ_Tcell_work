42a072acc87ea039feebdc7bc697384d80cb0a70
levin <dushiyi319@163.com>
2022-01-26 10:32:00 +0800

    some changes

--- 10xGenomics/scripts/clonotypes_add_consensus_annotations.py
+++ 10xGenomics/scripts/clonotypes_add_consensus_annotations.py
@@ -79,26 +79,27 @@ def main():
         else:
             usage()
             sys.exit()
+        output = open("clonotypes_add_consensus_annotations.csv", "w")
         
         clonotype_dict = read_clonotypes(file1)
         len_dict, detail_dict = read_consensus(file2)
 	# print(json.dumps(detail_dict, indent=4))
-        print("clonotype_id,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,fwr1,fwr1_nt,cdr1,cdr1_nt,fwr2,fwr2_nt,cdr2,cdr2_nt,fwr3,fwr3_nt,cdr3,cdr3_nt,fwr4,fwr4_nt,reads,umis,v_start,v_end,v_end_ref,j_start,j_start_ref,j_end,fwr1_start,fwr1_end,cdr1_start,cdr1_end,fwr2_start,fwr2_end,cdr2_start,cdr2_end,fwr3_start,fwr3_end,cdr3_start,cdr3_end,fwr4_start,fwr4_end,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,fwr1,fwr1_nt,cdr1,cdr1_nt,fwr2,fwr2_nt,cdr2,cdr2_nt,fwr3,fwr3_nt,cdr3,cdr3_nt,fwr4,fwr4_nt,reads,umis,v_start,v_end,v_end_ref,j_start,j_start_ref,j_end,fwr1_start,fwr1_end,cdr1_start,cdr1_end,fwr2_start,fwr2_end,cdr2_start,cdr2_end,fwr3_start,fwr3_end,cdr3_start,cdr3_end,fwr4_start,fwr4_end,frequency, proportion")
+        output.write("clonotype_id,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,fwr1,fwr1_nt,cdr1,cdr1_nt,fwr2,fwr2_nt,cdr2,cdr2_nt,fwr3,fwr3_nt,cdr3,cdr3_nt,fwr4,fwr4_nt,reads,umis,v_start,v_end,v_end_ref,j_start,j_start_ref,j_end,fwr1_start,fwr1_end,cdr1_start,cdr1_end,fwr2_start,fwr2_end,cdr2_start,cdr2_end,fwr3_start,fwr3_end,cdr3_start,cdr3_end,fwr4_start,fwr4_end,consensus_id,length,chain,v_gene,d_gene,j_gene,c_gene,full_length,productive,fwr1,fwr1_nt,cdr1,cdr1_nt,fwr2,fwr2_nt,cdr2,cdr2_nt,fwr3,fwr3_nt,cdr3,cdr3_nt,fwr4,fwr4_nt,reads,umis,v_start,v_end,v_end_ref,j_start,j_start_ref,j_end,fwr1_start,fwr1_end,cdr1_start,cdr1_end,fwr2_start,fwr2_end,cdr2_start,cdr2_end,fwr3_start,fwr3_end,cdr3_start,cdr3_end,fwr4_start,fwr4_end,frequency,proportion\n")
         for each in len_dict:
             if len(len_dict[each]) == 2:
                 if len_dict[each]['TRA'] == 1 and len_dict[each]['TRB'] == 1:
-                    print("{},{},{},{},{}".format(each,",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each,",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
                 elif len_dict[each]['TRA'] == 1 and len_dict[each]['TRB'] == 2:
-                    print("{},{},{},{},{}".format(each+".1",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
-                    print("{},{},{},{},{}".format(each+".2",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB_1']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each+".1",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each+".2",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB_1']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
                 elif len_dict[each]['TRA'] == 2 and len_dict[each]['TRB'] == 1:
-                    print("{},{},{},{},{}".format(each+".1",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
-                    print("{},{},{},{},{}".format(each+".2",",".join(detail_dict[each]['TRA_1']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each+".1",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each+".2",",".join(detail_dict[each]['TRA_1']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
                 elif len_dict[each]['TRA'] == 2 and len_dict[each]['TRB'] == 2:
-                    print("{},{},{},{},{}".format(each+".1",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
-                    print("{},{},{},{},{}".format(each+".2",",".join(detail_dict[each]['TRA_1']),",".join(detail_dict[each]['TRB_1']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))			
-                    print("{},{},{},{},{}".format(each+".3",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB_1']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
-                    print("{},{},{},{},{}".format(each+".4",",".join(detail_dict[each]['TRA_1']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))			
+                    output.write("{},{},{},{},{}\n".format(each+".1",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each+".2",",".join(detail_dict[each]['TRA_1']),",".join(detail_dict[each]['TRB_1']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))			
+                    output.write("{},{},{},{},{}\n".format(each+".3",",".join(detail_dict[each]['TRA']),",".join(detail_dict[each]['TRB_1']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))
+                    output.write("{},{},{},{},{}\n".format(each+".4",",".join(detail_dict[each]['TRA_1']),",".join(detail_dict[each]['TRB']),clonotype_dict[each]['freq'],clonotype_dict[each]['prop']))			
                     # print(each,len_dict[each],len(len_dict[each]))
 
 
