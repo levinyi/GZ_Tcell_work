b4a44006f2ec07f488c14fdfda4d42bdaf41aba3
levin <dushiyi319@163.com>
2022-03-29 11:03:18 +0800

    some changes

--- WES_pipeline/bin/DepthOfCoverage.step1.deal.data.R
+++ WES_pipeline/bin/DepthOfCoverage.step1.deal.data.R
@@ -5,13 +5,12 @@ library(ggplot2)
 # args = [GA13_TU_CL1-P3-gDNA.Tumor.bam.DepthOfCoverage.txt]
 args = commandArgs(T)
 
-
 for (each_file in args){
-	path_list = unlist(strsplit(each_file,"/"))
-	file_name = path_list[length(path_list)]
+  output = dirname(each_file)
+	file_name = basename(each_file)
 	title_name = paste(unlist(strsplit(file_name, "\\."))[1:2], collapse=".")
 	print(title_name)
-	data = read.csv(each_file, header=T)
+  data = read.csv(each_file, header=T)
 
 	data3 = data %>% separate(Locus,c("chr","pos"),":") %>%
 		# separate(chr,c("chr1","chr"),"chr") %>%  
@@ -20,5 +19,7 @@ for (each_file in args){
 			  total_depth = sum(Total_Depth),
 			  coverage_leng = sum(Total_Depth!=0)) %>%
 		mutate(rate1 = total_depth/chr_len, rate2=coverage_leng/chr_len)
-	write.table(data3, file=paste(title_name,"coverage.depth.rate.xls",sep="."), sep="\t", row.names = FALSE, quote=FALSE)
+	write.table(data3, file=paste(paste(output, title_name,sep = "/"),"DepthOfCoverage.rate.xls",sep="."), 
+	            sep="\t", row.names = FALSE, quote=FALSE)
 }
+print("all done")
