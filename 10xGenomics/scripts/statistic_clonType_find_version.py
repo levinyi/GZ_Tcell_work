cb70f69a0968a8a6596d61125d1e26f54e75fd20
levin <dushiyi319@163.com>
2022-07-11 08:36:29 +0800

    some changes

--- 10xGenomics/scripts/statistic_clonType_find_version.py
+++ 10xGenomics/scripts/statistic_clonType_find_version.py
@@ -4,15 +4,17 @@ import json
 # cellranger file
 
 # clonotype file
+VDJ_path = sys.argv[1]
+GEX_path = sys.argv[2]
+analysis_path = sys.argv[3]
 
-version_file = "/cygene2/pipeline/10X/data/HC29/G480E1_VDJ/_versions"
+version_file = VDJ_path+"/_versions"
+GEX_csv_file = GEX_path+"/outs/metrics_summary.csv"
+vdj_csv_file = VDJ_path+"/outs/metrics_summary.csv"
+analysis_file = analysis_path+"/merged_tcr_table.csv"
 with open(version_file, "r") as f:
     versions = json.load(f)
     print("cellranger: {}".format(versions['pipelines']))
-
-with open() as f:
-    versions = json.load(f)
-    print("clonotype: {}".format(versions['clonotype']))
 
 # sequence depth
 # before-QC total Cell number 
@@ -20,7 +22,26 @@ with open() as f:
 # Post-QC total Cell number
 # Post-QC total TCR pair number
 import pandas as pd
-csv_file = "/cygene2/pipeline/10X/data/HC29/G480E1_GEX/outs/metrics_summary.csv"
-data = pd.read_csv(csv_file)
-print("sequence depth: {}".format(data['sequence_depth'][0]))
-print("before-QC total Cell number: {}".format(data['before_qc_total_cell_number'][0]))
+GEX_data = pd.read_csv(GEX_csv_file)
+# print("sequence depth: {}".format(data['Estimated Number of Cells'][0]))
+print("before-QC total Cell number GEX: {}".format(GEX_data['Estimated Number of Cells'][0]))
+VDJ_data = pd.read_csv(vdj_csv_file)
+print("before-QC total Cell number VDJ: {}".format(VDJ_data['Estimated Number of Cells'][0]))
+
+# "raw_clonotype_id" used to find the unique clonotype
+# read the analysis file
+analysis_data = pd.read_csv(analysis_file)
+print("Paired TCR number: {}".format(len(pd.unique(analysis_data['raw_clonotype_id']))))
+
+CD4_file = analysis_path+"/CD4_Cluster_neoTCR.csv"
+CD8_file = analysis_path+"/CD8_Cluster_neoTCR.csv"
+CD4Treg_file = analysis_path+"/CD4_Treg_Cluster_neoTCR.csv"
+
+cd4_data = pd.read_csv(CD4_file,sep="\t")
+# print("cd4_data :", len(cd4_data))
+# print("cd4_data :", cd4_data.head())
+print("CD4 cell number: {}".format(len(cd4_data['clonotype'])))
+cd8_data = pd.read_csv(CD8_file, sep="\t")
+print("CD8 cell number: {}".format(len(cd8_data['clonotype'])))
+cd4treg_data = pd.read_csv(CD4Treg_file, sep="\t")
+print("CD4Treg cell number: {}".format(len(cd4treg_data['clonotype'])))
