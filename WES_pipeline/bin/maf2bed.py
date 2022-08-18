60b26bdfea7b174ad44131d2c806c5f0245c173a
levin <dushiyi319@163.com>
2022-08-18 08:50:07 +0800

    some changes

--- WES_pipeline/bin/maf2bed.py
+++ WES_pipeline/bin/maf2bed.py
@@ -1,17 +1,18 @@
 import sys
-import os
 import pandas as pd
 
 maf_file = sys.argv[1]
 
 # read in maf file
-
 data = pd.read_csv(maf_file, sep="\t",)
 print(data.head())
 
 for index, row in data.iterrows():
     # if chromosome is null pass
-    
-    if row['Chromosome'] == 'chrM':
+    if pd.isnull(row["Chromosome"]):
         continue
-    else
+    if row['Chromosome'] == 'chrM':
+        start = row['Start_Position']-1
+        end = row['End_Position'] -1
+
+print(row['Chromosome'], start, end, row['Tumor_Sample_Barcode'], sep="\t")
