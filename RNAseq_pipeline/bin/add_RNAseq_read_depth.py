5832b78d526d2b85221ea359f82a4b143346e496
levin <dushiyi319@163.com>
2022-08-19 09:39:24 +0800

    some changes

--- RNAseq_pipeline/bin/add_RNAseq_read_depth.py
+++ RNAseq_pipeline/bin/add_RNAseq_read_depth.py
@@ -10,6 +10,7 @@ def usage():
     print("""
         python {} <xls> <tpm file>
     Update:
+        20220818    fix format bug.
         20220725    re-write the function to print NAN to empty.
         20210410    add parmaters.
         20200703    Created.
@@ -44,7 +45,6 @@ def deal_mpileup(RNAseq_mpileup_file):
                 mutatype = mapping_info.upper().count(match_mut.group())
             else:
                 mutatype = 0
-            #wildtype = int(reads_count) - mutatype
             addtwodimdict(a_dict, pos, 'W', wildtype)
             addtwodimdict(a_dict, pos, 'M', mutatype)
             addtwodimdict(a_dict, pos, 'I', ins_type)
@@ -59,14 +59,23 @@ def main():
     mpileup_dict = deal_mpileup(RNAseq_mpileup_file)
 
     output_file = open(os.path.basename(xls_file).rstrip("xls")+'RNA_Depth.xls', "w")
-
-    data = pd.read_table(xls_file, sep="\t")
+    output_file2 = open(os.path.basename(xls_file).rstrip("xls")+'RNA_Depth.Error.Position.txt', "w")
+    output_file2.write("## Position not in mpileup file. please check!\n")
+    output_file2.write("Hugo_Symbol\tChromosome\tStart_Position\tTPM\n")
+    data = pd.read_table(xls_file, sep="\t", dtype=str)
     header = list(data.columns.values)
     header.extend(["MutatedReads(RNA)", "Wild-typeReads(RNA)"])
     output_file.write("{}\n".format("\t".join(header)))
     
+    data = data.replace(np.nan, "")
+    # print(data.head())
+    
     for index, row in data.iterrows():
-        row = row.replace(np.nan, "")
+        # if chromosome is empty, print the row
+        if row['Chromosome'] == "":
+            output_file.write("{}\n".format("\t".join([str(i) for i in row])))
+            continue
+
         Position_start = str(row["Start_Position"])
         Variant_Type = str(row['Variant_Type'])
         if Position_start in mpileup_dict:
@@ -78,14 +87,19 @@ def main():
             elif Variant_Type == 'INS':
                 RNA_muta_reads = int(mpileup_dict[Position_start].get("I", 0))
         else:
-            print("position start not in mpileup dict. please check! bug.") # this is a bug, should be fixed in the future.
+            if str(row['End_Position']) in mpileup_dict:
+                flag="End_Position_in_mpileup"
+            elif str(int(Position_start) -1) in mpileup_dict:
+                flag = "start_position-1_in_mpileup"
+            else:
+                flag = "both_Not_in_mpileup"
+            output_file2.write("{}\t{}\t{}\t{}\t{}\t{}\t\n".format(row["Hugo_Symbol"], row["Chromosome"], Position_start, row['End_Position'], flag, row["TPM"])) # this is a bug, should be fixed in the future.
             RNA_wild_reads = "NA"
             RNA_muta_reads = "NA"
         
         row["MutatedReads(RNA)"] = RNA_muta_reads
         row["Wild-typeReads(RNA)"] = RNA_wild_reads
         output_file.write("{}\n".format("\t".join([str(row[i]) for i in header])))
-    
     output_file.close()
 
 
