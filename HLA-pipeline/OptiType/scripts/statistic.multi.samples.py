9082c556f0e95233374d51c61e4775a57a226586
levin <dushiyi319@163.com>
2022-10-15 00:03:34 +0800

    some changes

--- HLA-pipeline/OptiType/scripts/statistic.multi.samples.py
+++ HLA-pipeline/OptiType/scripts/statistic.multi.samples.py
@@ -1,6 +1,11 @@
 import sys
+import os
 import pandas as pd
-for each_file in sys.argv[1:]:
-    filename = each_file.split()
-    data = pd.read_table(each_file, header=T)
+
+files = sys.argv[1:]
+for each_file in files:
+    filename = os.path.basename(each_file).split(".")[0]
+    df = pd.read_table(each_file, header=T)
+    print(df)
+    pd.melt(df, id_vars="",value_vars=[])
 
