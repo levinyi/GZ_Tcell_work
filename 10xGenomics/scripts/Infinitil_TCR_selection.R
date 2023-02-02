305a72c0de0ff14c1c24a693998216e6f57a34fd
levin <dushiyi319@163.com>
2023-02-02 12:09:11 +0800

    some changes

--- 10xGenomics/scripts/Infinitil_TCR_selection.R
+++ 10xGenomics/scripts/Infinitil_TCR_selection.R
@@ -1,5 +1,5 @@
 #' This script is used after scRNAseq Infinitil pipeline.
-
+setwd("/cygene/work/00.test/pipeline/10xGenomics/scripts")
 
 # generate a table
 
@@ -9,5 +9,8 @@ library(Seurat)
 #' output: table, unique clonotypes with cell-subtype information from GXP of CBs and FACS annotation.
 #' 
 #' 
+FOXP3 = 20
 
-tcr_stats = read.csv("tcr_stats_barcodes.csv")
+tcr_stats = read.csv("/cygene2/pipeline/10X/data/SA501001-TIL02/analysis/tcr_stats_barcodes.csv")
+tcr_stats[1:4,]
+tcr_stats %>% select(c("renamed_clone","TCR_clonality"))
